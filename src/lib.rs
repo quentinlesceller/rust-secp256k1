8324ac2873462b8ea4dd4db29272b37ecf6fb6e8
Quentin Le Sceller <q.lesceller@gmail.com>
2021-03-11 12:54:00 -0500

    Add rand_core to regular rand feature build

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,7 @@ pub use secp256k1_sys as ffi;
 #[cfg(feature = "bitcoin_hashes")] pub extern crate bitcoin_hashes;
 #[cfg(all(test, feature = "unstable"))] extern crate test;
 #[cfg(any(test, feature = "rand"))] pub extern crate rand;
-#[cfg(any(test))] extern crate rand_core;
+#[cfg(any(test, feature = "rand"))] extern crate  rand_core;
 #[cfg(feature = "serde")] pub extern crate serde;
 #[cfg(all(test, feature = "serde"))] extern crate serde_test;
 #[cfg(any(test, feature = "rand"))] use rand::Rng;
