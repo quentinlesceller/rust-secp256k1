526b86fd7cf664aee76dad0a3d9ee8c01901d3a4
Quentin Le Sceller <q.lesceller@gmail.com>
2021-03-11 12:49:38 -0500

    Change build.rs based on  https://github.com/mimblewimble/rust-secp256k1-zkp/pull/71

--- secp256k1-sys/build.rs
+++ secp256k1-sys/build.rs
@@ -27,6 +27,26 @@ use std::env;
 
 fn main() {
     // Actual build
+    if cfg!(feature = "external-symbols") {
+        println!("cargo:rustc-link-lib=static=secp256k1-zkp");
+        return;
+    }
+
+    // Check whether we can use 64-bit compilation
+    let use_64bit_compilation = if env::var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "64" {
+        let check = cc::Build::new().file("depend/check_uint128_t.c")
+                                    .cargo_metadata(false)
+                                    .try_compile("check_uint128_t")
+                                    .is_ok();
+        if !check {
+            println!("cargo:warning=Compiling in 32-bit mode on a 64-bit architecture due to lack of uint128_t support.");
+        }
+        check
+    } else {
+        false
+    };
+
+    // Actual Build
     let mut base_config = cc::Build::new();
     base_config.include("depend/secp256k1/")
                .include("depend/secp256k1/include")
@@ -34,6 +54,11 @@ fn main() {
                .flag_if_supported("-Wno-unused-function") // some ecmult stuff is defined but not used upstream
                .define("SECP256K1_BUILD", Some("1"))
                .define("ENABLE_MODULE_ECDH", Some("1"))
+               .define("ENABLE_MODULE_GENERATOR", Some("1"))
+               .define("ENABLE_MODULE_RECOVERY", Some("1"))
+               .define("ENABLE_MODULE_RANGEPROOF", Some("1"))
+               .define("ENABLE_MODULE_BULLETPROOF", Some("1"))
+               .define("ENABLE_MODULE_AGGSIG", Some("1"))
                .define("ENABLE_MODULE_SCHNORRSIG", Some("1"))
                .define("ENABLE_MODULE_EXTRAKEYS", Some("1"))
                .define("ECMULT_GEN_PREC_BITS", Some("4"))
@@ -47,6 +72,14 @@ fn main() {
     } else {
         base_config.define("ECMULT_WINDOW_SIZE", Some("15")); // This is the default in the configure file (`auto`)
     }
+
+
+    if let Ok(target_endian) = env::var("CARGO_CFG_TARGET_ENDIAN") {
+        if target_endian == "big" {
+            base_config.define("WORDS_BIGENDIAN", Some("1"));
+        }
+    }
+
     base_config.define("USE_EXTERNAL_DEFAULT_CALLBACKS", Some("1"));
     #[cfg(feature = "recovery")]
     base_config.define("ENABLE_MODULE_RECOVERY", Some("1"));
@@ -55,6 +88,16 @@ fn main() {
         "wasm32-unknown-unknown"|"wasm32-wasi" =>
             { base_config.include("wasm-sysroot"); },
         _ => {},
+    }
+
+
+    if use_64bit_compilation {
+        base_config.define("USE_FIELD_5X52", Some("1"))
+                   .define("USE_SCALAR_4X64", Some("1"))
+                   .define("HAVE___INT128", Some("1"));
+    } else {
+        base_config.define("USE_FIELD_10X26", Some("1"))
+                   .define("USE_SCALAR_8X32", Some("1"));
     }
 
     // secp256k1
